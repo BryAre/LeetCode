71ee0a63edf129cc598a3fa483af031d303c89a6
BryAre <37481349+BryAre@users.noreply.github.com>
2020-04-07 16:06:53 -0400

    added comments

--- 30DayChallenge/Day6.java
+++ 30DayChallenge/Day6.java
@@ -1,15 +1,33 @@
+
+/**
+ * Given an array of strings, group anagrams together.
+ */
+
 import java.util.*;
 
 class Day6 {
     public List<List<String>> groupAnagrams(String[] strs) {
-
+        // create a list that holds a list of strings (the anagrams)
         List<List<String>> anagrams = new ArrayList<>();
+
+        // create a hashmap
         HashMap<String, List<String>> map = new HashMap<>();
+
+        // grab each string, create an array of chars, sort the chars in alphabetical
+        // order,
+        // concatenate the chars together. Example) "CDBA" = 'C', 'D', 'B', 'A' = 'A',
+        // 'B', 'C','D' = "ABCD"
         for (String s : strs) {
             char[] sortedChars = s.toCharArray();
             Arrays.sort(sortedChars);
             String key = new String(sortedChars);
 
+            // we use the ordered string as a key. first we put it into the list if it is
+            // not in it.
+            // then we compare every other string to the ordered string, if they have the
+            // same
+            // letters, then we can add that string to the list of strings with the same
+            // order.
             if (!map.containsKey(key)) {
                 List<String> list = new ArrayList<>();
                 list.add(s);
@@ -17,6 +35,7 @@ class Day6 {
             } else
                 map.get(key).add(s);
         }
+
         anagrams.addAll(map.values());
         return anagrams;
 
